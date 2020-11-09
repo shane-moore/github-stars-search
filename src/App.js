a24e2e4d426520917cdbe5cf42579401c5dc8066
Shane <fullstack.fever@gmail.com>
2020-11-09 10:05:07 -0500

    tweaked styling

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  // Declare a new state variable, which we'll call "user"
+
   // useState is a hook that lets you add state to function components
-  const [user, setuser] = useState(''); // pass initial state and returns current state and function that updates it
+  // returns current state and function that updates it
+
+  // Declare a state variable to keep track of the searched user and the GitHub API's JSON response with the searched user's repo data
+  const [user, setuser] = useState('');
   const [currentRepos, setCurrentRepos] = useState([]);
 
-   // Similar to componentDidMount and componentDidUpdate:
+   // useEffect hook will call GitHub users REST API and will assign returned user's repo data as JSON after render and on change of the 'user' state
   useEffect(() => {
-    // Update the document title using the browser API
-    async function getStars(){
+    async function getReposByStars(){
       let nextPage = true;
       let repos = [];
       let page = 1;
+        // fetch data if user is being searched
         if(user){
+          // call API to get all the user's repos instead of just first 100
           while(nextPage) {
-          let response = await fetch(`https://api.github.com/users/${user}/repos?page=${page}&per_page=100`);
-          let json = await response.json();
-          // console.log(json)
-          // repos = Object.assign(repos, json);
-          repos.push(...json)
-          // console.log(repos)
-          let headers = response.headers.get('link');
-          if(headers &&headers.includes('rel=\"next\"')) {
-            console.log(response.headers.get('link'))
-            page++;
-            nextPage = true;
-          } else nextPage = false;
-      }
-    }
-    // console.log(repos);
-    repos = sortReposByStars(repos);
-    setCurrentRepos(repos);
-    // console.log(currentRepos)
-    // console.log(repos);
-    }
+            let response = await fetch(`https://api.github.com/users/${user}/repos?page=${page}&per_page=100`);
+            let json = await response.json();
+            repos.push(...json)
 
-    getStars();
-      },[user]);
+            // set nextPage variable to true if the response header shows that another page of user's content exists
+            let headers = response.headers.get('link');
+            if(headers &&headers.includes('rel=\"next\"')) {
+              page++;
+              nextPage = true;
+            } else nextPage = false;
+      }
+      console.log('Vote for Shane!')
+    }
+      // sort the repos by star count in desc order
+      repos = sortReposByStars(repos);
+
+      // update state of currentRepos
+      setCurrentRepos(repos);
+    }
+    // make API call when user's value changes
+    getReposByStars();
+    }, [user]);
 
   // sort user's repos by stars in repos
   function sortReposByStars(repos){
@@ -54,31 +56,39 @@ function App() {
   function repoLinkRender(repo) {
       return <a href={`${repo.html_url}`} target="blank">{repo.name}</a>
     }
-  return (
-    <section>
-      <header class="search-container">
-        <div class="invite">I want to view the most starred GitHub repositories created by <input type="text" id="search"/></div>
-        <button onClick={() => setuser(document.querySelector("#search").value)}>Search</button>
-      </header>
-      <div class='results-container'>
-        {/* <header>{currentRepos[0] ? 'Results': ''}</header> */}
-        <div class="results results__headers">
-          <p>{currentRepos[0] ? 'Repositories': ''}</p>
-          <p class="stars">{currentRepos[0] ? 'Stars': ''}</p>
-        </div>
-          {
-            currentRepos.map((repo,i) => (
-              <div class="results">
-                <p>{repo.name ? repoLinkRender(repo) : 'null'}</p>
-                <p class="stars">{repo.stargazers_count ? repo.stargazers_count : 0 }</p>
-              </div>
-            ))
-          }
-      </div>
-    </section>
 
+  // return repo and stars data as JSX to the mapping function
+
+  function resultsMap(repo) {
+    return (
+      <div className="results" key="repo.id">
+        <p>{repo.name ? repoLinkRender(repo) : 'null'}</p>
+        <p className="stars">{repo.stargazers_count ? repo.stargazers_count : 0 }</p>
+      </div>
+    );
+  }
+
+  // return some JSX to render
+  return (
+    <main>
+      <section>
+        <header className="search-container">
+          <div className="invite">I want to view the most starred GitHub repositories created by <input type="text" id="search"/></div>
+          <button onClick={() => setuser(document.querySelector("#search").value)}>Search</button>
+        </header>
+        <div className='results-container'>
+          <div className="results results__headers">
+            <p>{currentRepos[0] ? 'Repositories': ''}</p>
+            <p className="stars">{currentRepos[0] ? 'Stars': ''}</p>
+          </div>
+            {
+              currentRepos.map((repo) => {return resultsMap(repo)}
+              )
+            }
+        </div>
+      </section>
+    </main>
   );
 }
-
 
 export default App;
